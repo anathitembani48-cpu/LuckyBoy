ffed6d6f653bbd7e095b4ed3cfde2b892f928b53
anathitembani48-cpu <anathitembani48@gmail.com>
2026-06-09 03:31:35 +0200

    Update vite.config.ts

--- Lucky Learning/vite.config.ts
+++ Lucky Learning/vite.config.ts
@@ -5,3 +5,11 @@ import { tanstackStart } from '@tanstack/start/plugin/vite'
 export default defineConfig({
   plugins: [tanstackStart(), react()],
 })
+import netlify from '@netlify/vite-plugin-tanstack-start'
+
+export default defineConfig({
+  plugins: [
+    tanstackStart(),
+    netlify(),
+  ],
+})
